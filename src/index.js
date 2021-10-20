aa6e632b823aa5fff47a49d0cb7634312e63ecac
Romarjo Balukja <mbalukja1@gmail.com>
2021-10-20 15:29:50 +0200

    Add proccess port.

--- src/index.js
+++ src/index.js
@@ -69,6 +69,8 @@ app.get('*' , (req, res) => {
     res.send('Not found');
 });
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
     console.log('Server running at port 3000');
 });
